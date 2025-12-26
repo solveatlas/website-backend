137318bdf5143b111c7c834b33a1f926fca24fd3
Tilak-csd <tilakgubhaju3@gmail.com>
2025-12-27 00:07:39 +0545

    change the validator

--- middlewares/mailer.middlewares.js
+++ middlewares/mailer.middlewares.js
@@ -1,13 +1,7 @@
-const {validate} = require('deep-email-validator')
+const validator = require("validator");
 
-const verifyEmail = async (email) => {
-    const result = await validate({
-        email: email,
-        validateSMTP: false, // You can set this to false to see if SMTP is the culprit
-    });
-    
-    // console.log(result); // Check the "validators" object here
-    return result.valid;
-}
+const verifyEmail = (email) => {
+  return validator.isEmail(email);
+};
 
-module.exports = verifyEmail
+module.exports = verifyEmail;
